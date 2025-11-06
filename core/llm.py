1639e0662ee40e08762ae3a40fd31a7563736bc6
Shree-G <shree.hridai@gmail.com>
2025-11-05 22:59:50 -0500

    done w embeddings and settings files

--- core/llm.py
+++ core/llm.py
@@ -4,21 +4,11 @@ from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 from pydantic import Field, SecretStr
 from langchain_core.language_models.chat_models import BaseChatModel
-
-class LLMSettings(BaseSettings):
-    LLM_PROVIDER: str = Field("groq", env="LLM_PROVIDER")
-    LLM_TEMPERATURE: float = Field(0.0, env = "LLM_TEMPERATURE")
-    LLM_MODEL: str = Field("llama-3.3-70b-versatile", env = "LLM_MODEL")
-    GROQ_API_KEY: SecretStr = Field(..., env = "GROQ_API_KEY")
-
-    class Config():
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-        extra = "ignore"
+from app.settings import Settings
 
 def getLLM() -> BaseChatModel:
     try:
-        settings = LLMSettings()
+        settings = Settings()
     except Exception as e:
         print(f"There was an error with setting up your .env: {e}")
         raise
